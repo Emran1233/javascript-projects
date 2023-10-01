fc9b10356dbbb892b9891925e0bf143f48b6ccc3
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-10-01 14:15:19 -0500

    Update ObjectExercises.js

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -49,6 +49,21 @@ let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
 let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
 
 
+
+function fitnessTest(candidates){
+ let results = [], numSteps, turns;
+ for (let i = 0; i < candidates.length; i++){
+     numSteps = 0;
+     turns = 0;
+     while(numSteps < 20){
+     numSteps += candidates[i].move();
+     turns++;
+     }
+     results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+ }
+ return results;
+}
+
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
